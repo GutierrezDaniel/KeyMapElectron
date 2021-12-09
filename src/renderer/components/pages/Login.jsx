9ad9c9e16a20e9c9343c10be19337a4b51a5adfe
DanielGutierrez <daniel7dng@hotmail.com>
2021-12-08 21:47:58 -0300

    add material ui and roboto font

--- src/renderer/components/pages/Login.jsx
+++ src/renderer/components/pages/Login.jsx
@@ -2,35 +2,34 @@
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { setlogin, setlogout } from '../../state/loginSlice';
+import Button from '@mui/material/Button';
 
 const Login = () => {
   const { login } = useSelector((state) => state.login);
   const dispatch = useDispatch();
 
   const handleLogin = () => {
-    dispatch(setlogin({
-      login: true,
-      personalKey: '123456789',
-    }));
-  }
+    dispatch(
+      setlogin({
+        login: true,
+        personalKey: '123456789',
+      })
+    );
+  };
   const handleLogout = () => {
     dispatch(setlogout());
-  }
+  };
 
-  console.log({login});
+  console.log({ login });
   return (
     <div>
       <h1>Helowsiño</h1>
-      <button
-        onClick={handleLogin}
-      >
+      <Button variant="contained" onClick={handleLogin}>
         Login
-      </button>
-      <button
-        onClick={handleLogout}
-      >
+      </Button>
+      <Button variant="contained" onClick={handleLogout}>
         Logout
-      </button>
+      </Button>
     </div>
   );
 };
